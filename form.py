bb08acf120808a79aea4f024ce56940283c3a0e3
nivedha3111 <nivedhakesav@gmail.com>
2021-05-02 00:58:01 +0530

    ui

---
+++ form.py
@@ -0,0 +1,36 @@
+import json,time
+from flask import Flask, render_template, request, jsonify, Response
+import requests
+import base64,cv2
+
+
+app=Flask(__name__)
+output=[]#("message stark","hi")]
+@app.route('/')
+def home_page():
+    return render_template("index.html",result=output)
+
+
+@app.route('/result',methods=["POST","GET"])
+def Result():
+    if request.method=="POST":
+        print(list(request.form.values()))
+        result=list(request.form.values())[0]
+        if result.lower()=="restart":
+            output.clear()
+        else:
+            try:
+                r = requests.post('http://localhost:5055/webhook', json={"message": result})
+                print("Bot says, ")
+                for i in r.json():
+                    bot_message = i['text']
+                    print(f"{i['text']}")
+                output.extend([("message parker",result),("message stark",bot_message)])
+            except:
+                output.extend([("message parker", result), ("message stark", "We are unable to process your request at the moment. Please try again...")])
+
+        print(output)
+        return render_template("index.html",result=output)
+
+if __name__=="__main__":
+    app.run(debug=True)#,host="192.168.43.161")
